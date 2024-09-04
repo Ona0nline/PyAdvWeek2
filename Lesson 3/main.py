33da0eeae0986de008a2bd18615afef00e5ed8b6
Ona0nline <onasihle123@gmail.com>
2024-09-04 09:37:05 +0200

    Lesson 3

---
+++ Lesson 3/main.py
@@ -0,0 +1,12 @@
+with open('Lesson 3/cities.csv','w') as file:
+  
+  file.write('Pretoria,20 degrees celsius,sunny\n')
+  file.write('Strandfontein,10 degrees celsius,sunny\n')
+  
+  print(file)
+  
+  
+  
+  
+  
+  
